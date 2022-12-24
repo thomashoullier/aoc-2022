f911b0f2331c260be85a4286354994cfdbc12ee7
Thomas HOULLIER <pro@houllier.net>
2022-12-24 12:30:24 +0100

    graph directories are sorted in topological order

--- day_7/day7.cpp
+++ day_7/day7.cpp
@@ -9,6 +9,7 @@
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
+#include <boost/graph/topological_sort.hpp>
 
 /* Graph */
 struct VertexProperty {
@@ -17,7 +18,7 @@ struct VertexProperty {
   std::string name; // File or folder names.
 };
 
-typedef boost::adjacency_list<boost::listS, boost::listS,
+typedef boost::adjacency_list<boost::vecS, boost::vecS,
                               boost::bidirectionalS, VertexProperty> Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_desc;
 typedef boost::graph_traits<Graph>::vertex_iterator VItr;
@@ -111,15 +112,8 @@ void graph_builder::cd_tochild (const std::string &name) {
   boost::tie(out_i, out_end) = out_edges(cur_dir, g);
   auto find_res = std::find_if(out_i, out_end,
                                [this, name](GraphTraits::edge_descriptor e)
-                               { 
-                                std::cout << "Search: " << g[target(e, g)].name
-                                << " vs. " << name << std::endl;
-                                return g[target(e, g)].name == name; });
-  // TODO: Bug, we reach the end of the vector. Try printing all the out_edges.
-  std::cout << "Result distance to end: " << std::distance(out_end, find_res) << std::endl;
-  vertex_desc cd = target(*find_res, g);
-  std::cout << "cd to : " << g[cd].name << std::endl;
-  cur_dir = cd;
+                               { return g[target(e, g)].name == name; });
+  cur_dir = target(*find_res, g);
 }
 
 void graph_builder::add_file (const std::string name, const int size) {
@@ -136,15 +130,11 @@ void graph_builder::add_dir (const std::string name) {
   vertex_desc dir = add_vertex(g);
   g[dir].is_file = false;
   g[dir].name = name;
-  std::cout << "Add dir: " << name << std::endl;
-  std::cout << "Cur dir: " << g[cur_dir].name << std::endl;
   add_edge(cur_dir, dir, g);
-  std::cout << "Dir added." << std::endl;
 }
 
 void graph_builder::parse_cd_line (const std::string &line) {
   std::string cd_name = parse_cd_target(line);
-  std::cout << "cd target: " << cd_name << std::endl;
   if (cd_name == "..") { graph_builder::cd_toparent(); }
   else { graph_builder::cd_tochild(cd_name); }
 }
@@ -176,6 +166,45 @@ void graph_builder::process_line (const std::string &line) {
   else { std::cerr << "Line not recognized." << std::endl; }
 }
 
+/* Graph for computing and assessing folder sizes */
+class graph_sizes {
+public:
+  Graph g;
+  std::vector<vertex_desc> topo_dirs;
+  
+  graph_sizes (Graph _g);
+  
+  void sort_topo ();
+  //void compute_dirs_size ();
+  //void compute_contents_size (const vertex_desc &folder);
+};
+
+graph_sizes::graph_sizes (Graph _g) {
+/* Graph Sizes constructor */
+  g = _g;
+}
+
+void graph_sizes::sort_topo () {
+/* Sort the graph vertices in topological order */
+  std::vector<vertex_desc> topo_nodes;
+  topological_sort(g, std::back_inserter(topo_nodes));
+  std::copy_if(topo_nodes.begin(), topo_nodes.end(),
+               std::back_inserter(topo_dirs),
+               [this](vertex_desc v){return !g[v].is_file;});
+}
+
+/*
+void graph_sizes::compute_dirs_size () {
+/ Compute the size of all dirs in the tree /
+
+}*/
+
+/*
+void graph_sizes::compute_contents_size (const vertex_desc &folder) {
+/ Compute the size of the current folder's contents. /
+
+}*/
+
 int main (int argc, char *argv[]) {
   std::cout << "# Day 7#" << std::endl;
 
@@ -184,78 +213,32 @@ int main (int argc, char *argv[]) {
     return 1;
   }
 
-  graph_builder gb;
-  std::cout << "Graph builder created." << std::endl;
-
-  /* Parsing the input text */
-  std::ifstream input(argv[1]);
-  std::string line;
-  std::getline(input, line); // Skip first line (root folder)
-  while(std::getline(input, line)) {
-    std::cout << line << std::endl;
-    gb.process_line(line);
-  }
-
-  /* Minimal example for the graph usage.
-  Graph g;
-  
-  vertex_desc v0 = add_vertex(g);
-  vertex_desc v1 = add_vertex(g);
-  vertex_desc v2 = add_vertex(g);
-
-  g[v0].is_file = false;
-  g[v0].name = "/";
-  
-  g[v1].is_file = true;
-  g[v1].name = "file1";
-
-  g[v2].is_file = true;
-  g[v2].name = "file2";
-
-  add_edge(v0, v1, g);
-  add_edge(v0, v2, g);
-
-  VItr vitr, vend;
-  
-  boost::tie(vitr, vend) = vertices(g);
-  for ( ; vitr != vend ; ++vitr)
-    std::cout << g[*vitr].name << std::endl;
-
-
-  typename GraphTraits::in_edge_iterator in_i, in_end;
-  std::cout << "v2 in_edges: ";
-  for (boost::tie(in_i, in_end) = in_edges(v2,g); in_i != in_end; ++in_i) {
-    GraphTraits::edge_descriptor e = *in_i;
-    vertex_desc src = source(e, g), targ = target(e, g);
-    std::cout << "(" << g[src].name << ", " << g[targ].name << ") ";
-  }
-  std::cout << std::endl;
-
-  typename GraphTraits::out_edge_iterator out_i, out_end; 
-  std::cout << "v0 out_edges: ";
-  for (boost::tie(out_i, out_end) = out_edges(v0, g);
-       out_i != out_end; ++out_i){
-    GraphTraits::edge_descriptor e = *out_i;
-    vertex_desc src = source(e, g), targ = target(e, g);
-    std::cout << "(" << g[src].name << ", " << g[targ].name << ") ";
-  }
-  std::cout << std::endl;
-
-  std::cout << "Find child file1 in v0 children" << std::endl;
-  boost::tie(out_i, out_end) = out_edges(v0, g);
-  auto find_res = std::find_if(out_i, out_end,
-                               [g](GraphTraits::edge_descriptor e)
-                               { return g[target(e, g)].name == "file1"; });
-  vertex_desc found_vert = target(*find_res, g);
-  std::cout << g[found_vert].name << std::endl;
-  */
-
   /* Part 1 solution outline
  *   * Parse all folders and files into the graph.
  *     * Detect the type and subtype of lines: input line (cd or ls),
  *       output line (dir or file).
- *   * Iterate over all files (leaf nodes) and add their size to all of their
- *     parent nodes.
+ *   * Perform a topological sort of the directories in the graph.
+ *     Iterate from the bottom to the top, compute the size of
+ *     the directory's contents each time.
  *   * Iterate over folder nodes and accumulate the size of those
  *     with size at most X. */
+
+  /* Parsing the input text */
+  graph_builder gb;
+
+  std::ifstream input(argv[1]);
+  std::string line;
+  std::getline(input, line); // Skip first line (root folder)
+  while(std::getline(input, line)) {
+    gb.process_line(line);
+  }
+
+  /* Computing folder sizes */
+  graph_sizes gs (gb.g);
+  gs.sort_topo();
+  for (auto vert : gs.topo_dirs) {
+    std::cout << gs.g[vert].name << std::endl;
+  }
+
+  // TODO: Filter to obtain only folders.
 }
