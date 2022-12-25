8de3c87dd857d9c9f14eb63b5b6df5385bfda15b
Thomas HOULLIER <pro@houllier.net>
2022-12-25 15:46:34 +0100

    day8 p1 solved

--- day_8/day8.cpp
+++ day_8/day8.cpp
@@ -10,6 +10,7 @@
 #include <Eigen/Core>
 
 typedef Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> height_mat;
+typedef Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> visibility_map;
 
 std::vector<std::string> parse_input (std::ifstream &input) {
 /* Parse the input file to a vector of line strings */
@@ -49,6 +50,74 @@ height_mat parse_height_map (const std::vector<std::string> &str_vec) {
   return heights;
 }
 
+void probe_left_side (const height_mat &heights, visibility_map &visibs) {
+/* Mark the trees visible from the left side of the forest. */
+  for (int irow = 0; irow < heights.rows(); irow++) {
+    int max_height = -1;
+    for (int icol = 0; (icol < heights.cols() and max_height < 9); icol++) {
+      int cur_height = heights(irow, icol);
+      if (cur_height > max_height) {
+        max_height = cur_height;
+        visibs(irow, icol) = 1;
+      }
+    }
+  }
+}
+
+void probe_right_side (const height_mat &heights, visibility_map &visibs) {
+  for (int irow = 0; irow < heights.rows(); irow++) {
+    int max_height = -1;
+    for (int icol = heights.cols() - 1;
+         (icol >= 0 and max_height < 9); icol--) {
+      int cur_height = heights(irow, icol);
+      if (cur_height > max_height) {
+        max_height = cur_height;
+        visibs(irow, icol) = 1;
+      }
+    }
+  }
+}
+
+void probe_top_side (const height_mat &heights, visibility_map &visibs) {
+  for (int icol = 0; icol < heights.cols(); icol++) {
+    int max_height = -1;
+    for (int irow = 0; (irow < heights.rows() and max_height < 9); irow++) {
+      int cur_height = heights(irow, icol);
+      if (cur_height > max_height) {
+        max_height = cur_height;
+        visibs(irow, icol) = 1;
+      }
+    }
+  }
+}
+
+void probe_bottom_side (const height_mat &heights, visibility_map &visibs) {
+  for (int icol = 0; icol < heights.cols(); icol++) {
+    int max_height = -1;
+    for (int irow = heights.rows() - 1;
+         (irow >= 0 and max_height < 9); irow--) {
+      int cur_height = heights(irow, icol);
+      if (cur_height > max_height) {
+        max_height = cur_height;
+        visibs(irow, icol) = 1;
+      }
+    }
+  }
+}
+
+visibility_map compute_visibility_map (const height_mat &heights) {
+/* Compute the visibility map corresponding to the tree heights matrix */
+  visibility_map visibs;
+  visibs = visibility_map::Constant(heights.rows(), heights.cols(), 0);
+  // We probe the four sides of the matrix with an eye. We mark trees
+  // as visible from the edges inward.
+  probe_left_side(heights, visibs);
+  probe_right_side(heights, visibs);
+  probe_top_side(heights, visibs);
+  probe_bottom_side(heights, visibs);
+  return visibs;
+}
+
 int main (int argc, char *argv[]) {
   std::cout << "# Day 8#" << std::endl;
 
@@ -61,4 +130,11 @@ int main (int argc, char *argv[]) {
   std::ifstream input(argv[1]);
   auto str_vec = parse_input(input);
   auto heights = parse_height_map(str_vec);
+
+  /* Probing for visible trees */
+  auto visibs = compute_visibility_map(heights);
+  
+  /* Counting visible trees */
+  int nvis = visibs.sum();
+  std::cout << "Visible trees: " << nvis << std::endl;
 }
