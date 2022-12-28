7a52e26073587216f2e18feb3ff2e26eea2f940f
Thomas HOULLIER <pro@houllier.net>
2022-12-28 08:59:50 +0100

    day9 structure

--- day_9/day9.cpp
+++ day_9/day9.cpp
@@ -9,6 +9,7 @@
 
 #include <boost/functional/hash.hpp>
 
+/* Position structure */
 struct pos {
   int row;
   int col;
@@ -31,6 +32,85 @@ struct pos {
     }
   };
 };
+
+/* Move struct */
+struct move {
+  char dir;
+  int steps;
+
+  move(char dir, int steps) {
+    this->dir = dir;
+    this->steps = steps;
+  }
+};
+
+move parse_moveline (const std::string &line) {
+/* Parse a move string */
+  char dir = line.at(0);
+  int steps = line.at(2) - '0';
+  return move(dir, steps);
+}
+
+/* Head and Tail knots state class */
+typedef std::unordered_map<pos, int, pos::hash_function> pos_counts_map;
+
+class knots_state {
+  public:
+  pos head;
+  pos tail;
+  pos_counts_map visits;
+
+  knots_state ();
+  void process_move (const move &_move);
+  
+  private:
+  pos move_to_pos (const move &_move); // Convert the move direction to a delta pos.
+  void update_head (const pos &dpos); // Update the head position.
+  void update_tail (); // Update the tail position.
+  void note_visited (); // Update the tail visit map.
+};
+
+knots_state::knots_state () {
+  head = pos(0, 0);
+  tail = pos(0, 0);
+  //TODO: tail visited 0,0
+}
+
+pos knots_state::move_to_pos (const move &_move) {
+  switch (_move.dir) {
+    case 'L': return pos(0, -1);
+    case 'R': return pos(0, 1);
+    case 'U': return pos(1, 0);
+    case 'D': return pos(-1, 0);
+    default: std::cerr << "Move direction unrecognized." << std::endl;
+  }
+}
+
+knots_state::update_head (const pos &dpos) {
+/* Update the head position by a single step */
+  head.row += dpos.row;
+  head.col += dpos.col;
+}
+
+knots_state::update_tail () {
+/* Update the tail position in relation with the head position */
+
+}
+
+knots_state::note_visited () {
+/* Update the visits map with the current tail position */
+
+}
+
+knots_state::process_move (const move &_move) {
+/* Process a single move line */
+  pos dpos = move_to_pos(_move); // Single update step.
+  for (istep = 0; istep < _move.steps; istep++) {
+    update_head(dpos);
+    update_tail();
+    note_visited();
+  }
+}
 
 int main (int argc, char *argv[]) {
   std::cout << "# Day 9#" << std::endl;
